5c916cc208a961b3651b9d466fb7e6826cdbd11f
Alpha Shuro <alphashuro@gmail.com>
2022-05-17 19:21:57 +0200

    Update run_once_0-install-brew.sh

--- run_once_0-install-brew.sh
+++ run_once_0-install-brew.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "installing brew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ${HOME}/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -8,11 +8,11 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 # Brewfile hash: {{ include "Brewfile" | sha256sum }}
 
 echo "running brew bundle install..."
-brew bundle install
+NONINTERACTIVE=1 brew bundle install --file ~/Brewfile
 echo "running brew bundle install DONE."
 
 echo "starting goku service..."
-brew services start goku
+NONINTERACTIVE=1 brew services start goku
 echo "starting goku service DONE."
 
 echo "installing brew DONE."
