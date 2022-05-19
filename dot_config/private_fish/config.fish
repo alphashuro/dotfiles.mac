cab62b244932303ba96769d00f627fb3da06ee23
Alpha Shuro <alphashuro@gmail.com>
2022-05-19 08:37:41 +0200

    update fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     set -x EDITOR nvim
-    source /usr/local/share/autojump/autojump.fish
+    source /opt/homebrew/share/autojump/autojump.fish
 
     starship init fish | source
 end
