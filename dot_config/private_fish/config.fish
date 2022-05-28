3ac30cd6b97ab67cd074e840efa8167eda9c37b6
Alpha Shuro <alphashuro@gmail.com>
2022-05-28 18:07:18 +0200

    Use dynamic brew prefix

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     set -x EDITOR nvim
-    source /opt/homebrew/share/autojump/autojump.fish
+    source (brew --prefix)/share/autojump/autojump.fish
 
     starship init fish | source
+
+    set LIBRARY_PATH $LIBRARY_PATH (brew --prefix)/lib (brew --prefix)/opt/libiconv/lib
 end
