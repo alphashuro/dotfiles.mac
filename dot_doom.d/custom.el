3aa5eda2de4dac0d4c990aa544fbac834f9d0a02
Alpha Shuro <alphashuro@gmail.com>
2022-07-17 14:49:56 +0200

    add doom emacs config

---
+++ dot_doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("5784d048e5a985627520beb8a101561b502a191b52fa401139f4dd20acb07607" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
